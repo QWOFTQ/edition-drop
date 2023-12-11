2b0a1a26166b386c0f4da8c24292c29c3aa26640
unknown <rkdcofls3890@naver.com>
2023-12-12 01:23:18 +0900

    first commit

---
+++ const/yourDetails.ts
@@ -0,0 +1,6 @@
+// Replace this with your contract address
+export const myEditionDropContractAddress: string =
+  "0xeBfe9fc6711dd0b809483Ea143c4c9F6909dAE2D"
+
+// Replace this with your token id
+export const tokenId: string = "1"
